89541a019f2e9bf42851d532c28669ba6d682a8b
NIbir888 <nibir.sakib111@gmail.com>
2026-06-15 11:39:24 +0600

    Input Jome

--- Assets/Samples/BovineLabs Grove/2.1.0-exp/Sample/Scripts/BovineLabs.Grove.Sample.Authoring/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
-// Copyright (c) DefaultCompany. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("BovineLabs.Grove.Sample.Editor")]
-[assembly: InternalsVisibleTo("BovineLabs.Grove.Sample.Tests")]
--- Assets/Samples/BovineLabs Grove/2.1.0-exp/Sample/Scripts/BovineLabs.Grove.Sample/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
-// Copyright (c) DefaultCompany. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("BovineLabs.Grove.Sample.Debug")]
-[assembly: InternalsVisibleTo("BovineLabs.Grove.Sample.Editor")]
-[assembly: InternalsVisibleTo("BovineLabs.Grove.Sample.Tests")]
