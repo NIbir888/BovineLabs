5271bce0a1194ac18991c90b71e62e2430b5b850
NIbir888 <nibir.sakib111@gmail.com>
2026-06-26 16:52:14 +0600

    Fixed Object Definition

--- Assets/Scripts/Scripts.Data/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Scripts")]
-[assembly: InternalsVisibleTo("Scripts.Authoring")]
-[assembly: InternalsVisibleTo("Scripts.Debug")]
-[assembly: InternalsVisibleTo("Scripts.Editor")]
-[assembly: InternalsVisibleTo("Scripts.Tests")]
--- Assets/Scripts/Scripts/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Scripts.Debug")]
-[assembly: InternalsVisibleTo("Scripts.Editor")]
-[assembly: InternalsVisibleTo("Scripts.Tests")]
